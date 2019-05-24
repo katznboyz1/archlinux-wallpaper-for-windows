ff8f51ccf1f7ed0fadd7d56057446d0154bf0e0c
katznboyz <38505228+katznboyz1@users.noreply.github.com>
2019-05-24 15:18:40 -0400

    Added a weather bar

--- archlinux-style-wallpaper.py
+++ archlinux-style-wallpaper.py
@@ -1,5 +1,5 @@
 #import statements
-import time, PIL, PIL.Image, PIL.ImageDraw, PIL.ImageFont, os, ctypes, datetime, sys, psutil
+import time, PIL, PIL.Image, PIL.ImageDraw, PIL.ImageFont, os, ctypes, datetime, sys, psutil, urllib.request
 import json as JSON
 from time import gmtime, strftime
 
@@ -21,9 +21,10 @@ class program:
                 ENDTIMEDICT['hour_12HR'] = int(LOCALTIME[partition])
                 if (int(ENDTIMEDICT['hour_24HR'] > 12)):
                     ENDTIMEDICT['hour_12HR'] = int(LOCALTIME[partition]) - 12
-                    ENDTIMEDICT['pm/am'] = 'pm'
-                else:
+                if (int(ENDTIMEDICT['hour_24HR']) < 12):
                     ENDTIMEDICT['pm/am'] = 'am'
+                else:
+                    ENDTIMEDICT['pm/am'] = 'pm'
             elif (partition == 4):
                 ENDTIMEDICT['minute'] = LOCALTIME[partition]
             elif (partition == 5):
@@ -84,16 +85,26 @@ class program:
     def main() -> None:
         manifestData = program.getPresetData()
         image = PIL.Image.open(manifestData['data']['image-path'])
+        weatherData = {'location':manifestData['data']['closest-city'], 'weather-get-success':True, 'temperature':0, 'weather':0}
+        try:
+            dataForWeatherData = JSON.loads(str(urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units=imperial'.format(manifestData['data']['closest-city'], manifestData['data']['owm-api-key'])).read().decode()))
+            weatherData['weather'] = dataForWeatherData['weather'][0]['main'].capitalize()
+            weatherData['temperature'] = dataForWeatherData['main']['temp']
+        except Exception as error:
+            print ('Error while getting weather information: {}'.format(error))
         columns = {
             'Time':[str(program.formatTimeString('%hour%:%minute%%pm/am%')), str(program.formatTimeString('%timeofdayname%')).lower().capitalize()],
             'Day':[str(program.formatTimeString('%weekdayname%')), str(program.formatTimeString('%monthname% %day%'))],
             'System':[str(sys.platform), str('Cpu: {}%'.format(str(psutil.cpu_percent()))), str('Mem: {}%'.format(psutil.virtual_memory().percent))]
         }
         topicsAlphabeticallySorted = ['Day', 'System', 'Time'] #fix this so it automatically sorts and you dont have to hardcode it
+        if (weatherData['weather-get-success']):
+            topicsAlphabeticallySorted.append('Weather')
+            columns['Weather'] = [str(weatherData['location']).capitalize(), str('Temperature: {}°F'.format(str(weatherData['temperature']))), str('Weather: {}'.format(str(weatherData['weather'])))]
         marginWidth = 10
         seperatorLineWidth = 1
-        columnHeights = int(image.size[1] / 6)
-        marginLeft = int((image.size[0] / 3) / 2)
+        columnHeights = int(image.size[1] / 8)
+        marginLeft = int((image.size[0] / 4) / 2)
         marginBottom = int((image.size[1] - columnHeights) / 2)
         imageFreeContentAreaWidth = int(image.size[0] - (marginLeft * 2))
         image = image.rotate(-90, expand = 1)
